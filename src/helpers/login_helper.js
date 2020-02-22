08846c1ec4156d7ffab65f51faf0b224cde8e368
João Victor Figueiredo Rodrigues <joao.victor21595@gmail.com>
2020-02-22 16:03:00 -0300

    criada um helpers de usuario

---
+++ src/helpers/login_helper.js
@@ -0,0 +1,18 @@
+class db_Manage{
+    constructor (email, senha){
+        this.email = email
+        this.senha = senha
+    }
+    async login(email, senha){
+        if(this.email == email && this.senha == senha){
+            return true
+        }
+        if(this.email != email){
+            return false
+        }
+        if(this.senha != senha){
+            return false
+        }
+    }
+}
+module.exports = db_Manage
