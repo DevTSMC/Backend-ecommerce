225403c250f95eba3cc82bde73027bd0d8fd23d8
João Victor Figueiredo Rodrigues <joao.victor21595@gmail.com>
2020-02-22 16:03:41 -0300

    criado a classe de producao para fazer manipulaçao dos usuarios e os tests dela

---
+++ src/usuarios/login.js
@@ -0,0 +1,22 @@
+const User = require("../helpers/login_helper")
+class Login {
+    constructor (email, senha){
+        this.user = new User(email, senha)
+    }
+    async login(httpRequest){
+        const {email, senha}  = httpRequest.body
+        if (!email || !senha){
+            return {statusCode: 401, message: "Email ou Senha Null" }
+        }
+        if(await this.user.login(email,senha) == true){
+            return {statusCode: 200 }
+        }
+        if(await this.user.login(email,senha) == false){
+            return {statusCode: 401, message: "Email invalido" }
+        }
+        if(await this.user.login(email,senha) == false){
+            return {statusCode: 401, message: "Senha invalido" }
+        }
+    }
+}
+module.exports = Login
