7fa1ff0fb4a8db3e85378f3af744d20401d580ac
João Victor Figueiredo Rodrigues <joao.victor21595@gmail.com>
2020-02-24 17:53:21 -0300

    fixado para test de integracao

--- src/helpers/login_helper.js
+++ src/helpers/login_helper.js
@@ -1,18 +1,34 @@
+const { User } = require('../models')
+
 class DbManage {
-  constructor (email, senha) {
-    this.email = email
-    this.senha = senha
+  constructor (email, senha, test) {
+    if (test === true) {
+      this.email = email
+      this.senha = senha
+      this.test = test
+    }
   }
 
   async login (email, senha) {
-    if (this.email === email && this.senha === senha) {
+    if (this.test === true) {
+      if (this.email === email && this.senha === senha) {
+        return true
+      }
+      if (this.email !== email) {
+        return false
+      }
+      if (this.senha !== senha) {
+        return false
+      }
+    } else {
+      const user = await User.findOne({ where: { email } })
+      if (!user) {
+        return false
+      }
+      if (!(await user.checkPassword(senha))) {
+        return false
+      }
       return true
-    }
-    if (this.email !== email) {
-      return false
-    }
-    if (this.senha !== senha) {
-      return false
     }
   }
 }
