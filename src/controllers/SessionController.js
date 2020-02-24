db7b861dd824249d951f1905e20f92bd8a14183d
João Victor Figueiredo Rodrigues <joao.victor21595@gmail.com>
2020-02-24 17:52:30 -0300

    adicionado a rota de login no controller

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -2,6 +2,13 @@ const User = require('../usuarios/login')
 
 class SessionController {
   async login (req, res) {
-
+    const { email, senha } = req.body
+    const user = new User(email, senha, false)
+    const login = await user.login(req)
+    if (login.statusCode === 200) {
+      return res.status(200).send()
+    }
+    return res.status(login.statusCode).json({ message: login.message })
+  }
 }
 module.exports = new SessionController()
