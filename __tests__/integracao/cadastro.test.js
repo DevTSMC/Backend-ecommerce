e7e0556159ea99e83103d5a399e170587de23096
jefesonk1 <jefesonk1@outlook.com>
2020-02-26 21:02:38 -0300

    deletado arquivo indesejado/adicionado campo name para testes

--- __tests__/integracao/cadastro.test.js
+++ __tests__/integracao/cadastro.test.js
@@ -6,7 +6,7 @@ const { User } = require('../../src/models')
 const makeSUT = async () => {
   class CreateUser {
     async create () {
-      const user = await User.create({ email: 'email_valido@email.com', senha: 'senha_valida', name: 'Tabocinha' })
+      const user = await User.create({ email: 'correto@email.com', senha: 'Aa123abc@@', name: 'Maria Luiza' })
       return user
     }
   }
@@ -14,53 +14,59 @@ const makeSUT = async () => {
   await user.create()
 }
 
-describe('Login', () => {
+describe('Cadastro', () => {
   beforeEach(async () => {
     await truncate()
   })
   test('deve retornar 401 se email nao for passado', async () => {
     const response = await request(app)
-      .post('/login')
+      .post('/register')
       .send({
-        senha: 'senha_valida'
+        nome: 'Maria Luiza',
+        senha: 'Aa123abc@@'
       })
     expect(response.status).toBe(401)
   })
-  test('deve retornar 401 se senha nao for passada', async () => {
+
+  test('deve retornar 401 se nome nao for passado', async () => {
     const response = await request(app)
-      .post('/login')
+      .post('/register')
       .send({
-        email: 'email_valido@email.com'
+        email: 'correto2@email.com',
+        senha: 'Aa123abc@@'
       })
     expect(response.status).toBe(401)
   })
-  test('deve retornar 401 se se email for invalido', async () => {
+
+  test('deve retornar 401 se senha nao for passado', async () => {
+    const response = await request(app)
+      .post('/register')
+      .send({
+        nome: 'Maria Luiza',
+        email: 'correto2@email.com'
+      })
+    expect(response.status).toBe(401)
+  })
+
+  test('deve retornar 401 se email já existe', async () => {
     await makeSUT()
     const response = await request(app)
-      .post('/login')
+      .post('/register')
       .send({
-        email: 'email_invalido@email.com',
-        senha: 'senha_valida'
+        nome: 'Maria Luiza',
+        email: 'correto@email.com',
+        senha: 'Aa123abc@@'
       })
     expect(response.status).toBe(401)
   })
-  test('deve retornar 401 se se senha for invalida', async () => {
+  test('deve retornar 200 se cadastrado com sucesso', async () => {
     await makeSUT()
     const response = await request(app)
-      .post('/login')
+      .post('/register')
       .send({
-        email: 'email_valido@email.com',
-        senha: 'senha_invalida'
-      })
-    expect(response.status).toBe(401)
-  })
-  test('deve retornar 200 se as credenciais forem validas', async () => {
-    await makeSUT()
-    const response = await request(app)
-      .post('/login')
-      .send({
-        email: 'email_valido@email.com',
-        senha: 'senha_valida'
+        nome: 'Maria Luiza',
+        email: 'correto2@email.com',
+        senha: 'Aa123abc@@'
       })
     expect(response.status).toBe(200)
   })
