3cfe84214bf9752ca4f3cfd3c5f801ef6c86768e
jefesonk1 <jefesonk1@outlook.com>
2020-02-26 21:07:38 -0300

    adicionado o helpers para cadastro de usuarios

---
+++ src/helpers/register_helper.js
@@ -0,0 +1,29 @@
+const { User } = require('../models')
+
+class DbManage {
+  constructor (name, email, senha, test) {
+    if (test === true) {
+      this.name = name
+      this.email = email
+      this.senha = senha
+      this.test = test
+    }
+  }
+
+  async register (name, email, senha) {
+    if (this.test === true) {
+      if (this.email === email) {
+        return false
+      }
+    } else {
+      const user = await User.findOne({ where: { email } })
+      if (!user) {
+        const result = await User.create({ email, senha, name })
+        console.log(result)
+        return true
+      }
+      return false
+    }
+  }
+}
+module.exports = DbManage
