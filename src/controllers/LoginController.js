3c086623091029cb1ee19718d1c4e8073c65176f
João Victor Figueiredo Rodrigues <joao.victor21595@gmail.com>
2020-02-28 17:20:28 -0300

    fix bug

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -1,11 +1,5 @@
 const User = require('../usuarios/login')
-<<<<<<< HEAD:src/controllers/SessionController.js
-const CreateUser = require('../usuarios/cadastro')
-
-class SessionController {
-=======
 class LoginController {
->>>>>>> feature/0.7-DeleteUser:src/controllers/LoginController.js
   async login (req, res) {
     const { email, senha } = req.body
     const user = new User(email, senha, false)
@@ -14,13 +8,6 @@ class LoginController {
       return res.status(200).send()
     }
     return res.status(login.statusCode).json({ message: login.message })
-  }
-
-  async cadastrar (req, res) {
-    const { nome, email, senha } = req.body
-    const user = new CreateUser(nome, email, senha, false)
-    const userC = await user.cadastro(req)
-    return res.status(userC.statusCode).json({ message: userC.message })
   }
 }
 module.exports = new LoginController()
