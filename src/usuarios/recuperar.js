6831fdc19f88ea5c4752bd3171eb0fc8872aea5c
João Victor Figueiredo Rodrigues <joao.victor21595@gmail.com>
2020-02-29 00:46:28 -0300

    adicionado rota de recuperar senha

---
+++ src/usuarios/recuperar.js
@@ -0,0 +1,20 @@
+const User = require('../helpers/recuperar_helper')
+class Recuperar {
+  constructor (email, test) {
+    this.user = new User(email, test)
+  }
+
+  async reset (httpRequest) {
+    const { email } = httpRequest.body
+    try {
+      const { resultado, novaSenha } = await this.user.reset(email)
+      if (resultado === true) {
+        return { statusCode: 200, novaSenha, message: 'Senha resetada' }
+      }
+      return { statusCode: 401, message: 'Email nao encontrado' }
+    } catch (error) {
+      return { statusCode: 401, message: 'Senha invalido' }
+    }
+  }
+}
+module.exports = Recuperar
