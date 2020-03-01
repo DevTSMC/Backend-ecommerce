db95bfea79708391f9b8d9c1ccaac5b1e43a3249
João Victor Figueiredo Rodrigues <joao.victor21595@gmail.com>
2020-03-01 16:24:01 -0300

    adicionado rota,controller e helper para produto

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,14 @@ const DeleteController = require('./controllers/DeleteController')
 const RegisterController = require('./controllers/RegisterController')
 const EditController = require('./controllers/EditController')
 const ResetController = require('./controllers/ResetController')
+const GetProdutosController = require('./controllers/GetProdutosController')
 
 routes.post('/login', LoginController.login)
 routes.post('/delete', DeleteController.delete)
 routes.post('/register', RegisterController.cadastrar)
 routes.post('/edit', EditController.Edit)
 routes.post('/reset', ResetController.reset)
+
+routes.get('/getProdutos', GetProdutosController.get)
 
 module.exports = routes
